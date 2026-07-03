3b6fd544067e10b1b6f89513d87d9ddc8d44d622
hauveli <136408450+hauveli@users.noreply.github.com>
2026-07-03 19:46:34 +0200

    I still have no idea what I'm doing

---
+++ doc/src/hexdoc_fishcasting/pages.py
@@ -0,0 +1,9 @@
+# .venv\Lib\site-packages\hexdoc_hexcasting\book\recipes.py
+# .venv\Lib\site-packages\hexdoc_hexcasting\_templates\recipes\hexcasting\brainsweep.html.jinja
+
+from hexdoc.patchouli.page import EmptyPage
+from .recipe import VoidsweepRecipe
+from hexdoc_hexcasting.book.page.pages.BrainsweepPage
+
+class VoidsweepRecipePage(BrainsweepPage):
+    pass
---
+++ doc/src/hexdoc_fishcasting/recipes.py
@@ -0,0 +1,11 @@
+# .venv\Lib\site-packages\hexdoc_hexcasting\book\recipes.py
+# .venv\Lib\site-packages\hexdoc_hexcasting\_templates\recipes\hexcasting\brainsweep.html.jinja
+
+
+from hexdoc.minecraft.recipe import ItemIngredientList, ItemResult, Recipe
+from pydantic import Field
+from hexdoc_hexcasting.book.recipes import EntityTypeIngredient
+
+
+class VoidsweepRecipe(EntityTypeIngredient):
+    pass
