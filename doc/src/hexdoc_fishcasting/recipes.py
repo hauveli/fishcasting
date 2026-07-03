31006c122f08877b4a484f8e0a1c51410ffaf57a
hauveli <136408450+hauveli@users.noreply.github.com>
2026-07-03 19:59:16 +0200

    ok this then

--- doc/src/hexdoc_fishcasting/recipes.py
+++ doc/src/hexdoc_fishcasting/recipes.py
@@ -1,7 +1,6 @@
 # .venv\Lib\site-packages\hexdoc_hexcasting\book\recipes.py
 # .venv\Lib\site-packages\hexdoc_hexcasting\_templates\recipes\hexcasting\brainsweep.html.jinja
 
-
 from hexdoc.minecraft.recipe import ItemIngredientList, ItemResult, Recipe
 from pydantic import Field
 from hexdoc_hexcasting.book.recipes import EntityTypeIngredient
