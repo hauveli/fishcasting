835ccfff3648b5ff8116165aea47b48bc0ad87ac
hauveli <136408450+hauveli@users.noreply.github.com>
2026-09-12 18:22:52 +0200

    Wip bobber

--- src/common/main/kotlin/hauveli/fishcasting/features/trader/BlessedTrades.kt
+++ src/common/main/kotlin/hauveli/fishcasting/features/trader/BlessedTrades.kt
@@ -189,8 +189,8 @@ object BlessedTrades {
 
     private val ENDGAME_TRADES_FISH = arrayOf<VillagerTrades.ItemListing>(
         fishcastingFishTrade(TideFish.ALPHA_FISH, FishcastingItems.HEXXY_FOCUS_BOBBER.value),
-        fishcastingFishTrade(TideFish.LUMINESCENT_JELLYFISH, FishcastingItems.SLICK_BAIT.value),
-        fishcastingFishTrade(TideFish.GILDED_MINNOW, FishcastingItems.TINY_BAIT.value),
+        fishcastingFishTrade(TideFish.LUMINESCENT_JELLYFISH, FishcastingItems.SLICK_BAIT.value, count = 6),
+        fishcastingFishTrade(TideFish.GILDED_MINNOW, FishcastingItems.TINY_BAIT.value, count = 11),
         fishcastingFishTrade(TideFish.URANIAS_PISCES, wantTool = false, item = HexItems.SPELLBOOK.get())
         // fishcastingFishTrade(TideFish.MAGMA_MACKEREL, FishcastingItems.TACKLEBOX_CHAIR_AERONAUTICS.value),
     )
