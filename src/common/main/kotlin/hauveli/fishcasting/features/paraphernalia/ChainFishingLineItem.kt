89c1be124fd0a0c3bef14b336eac2dec405b82de
hauveli <136408450+hauveli@users.noreply.github.com>
2026-09-22 23:09:29 +0200

    a lot of wip on the book oh my goodness there is a lot

---
+++ src/common/main/kotlin/hauveli/fishcasting/features/paraphernalia/ChainFishingLineItem.kt
@@ -0,0 +1,6 @@
+package hauveli.fishcasting.features.paraphernalia
+
+import com.li64.tide.registries.items.FishingLineItem
+
+class ChainFishingLineItem(properties: Properties?) :
+    FishingLineItem(properties, "item.fishcasting.chain_fishing_line.desc")
